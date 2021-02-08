ea9ef7a67d797df3149e3d9f514aae219ff2a7c2
spvessel <spvessel@gmail.com>
2021-02-08 15:35:12 +0300

    Removed CSharp version of SpaceVIL.

--- CSharp/SpaceVIL/SpaceVIL/Core/ITextEditable.cs
+++
@@ -1,7 +0,0 @@
-namespace SpaceVIL.Core
-{
-    /// <summary>
-    /// An interface that describes elements containing editable text.
-    /// </summary>
-    public interface ITextEditable { }
-}
--- CSharp/SpaceVIL/SpaceVIL/Core/IWindowAnchor.cs
+++
@@ -1,9 +0,0 @@
-namespace SpaceVIL.Core
-{
-    /// <summary>
-    /// An interface that defines the draggable window type of an item.
-    /// </summary>
-    public interface IWindowAnchor
-    {
-    }
-}

daef59fcf03ec138cffcc0a36610315dd7902d3a
VSedaikina <VSedaikina@gmail.com>
2022-09-20 23:47:46 +0300

    Tab manipulations on mouse button left only.

--- spacevil/src/main/java/com/spvessel/spacevil/TabBar.java
+++ spacevil/src/main/java/com/spvessel/spacevil/TabBar.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.spvessel.spacevil.Core.IBaseItem;
-// import com.spvessel.spacevil.Core.AbstractHorizontalLayout;
 import com.spvessel.spacevil.Core.IHLayout;
 import com.spvessel.spacevil.Core.MouseArgs;
+import com.spvessel.spacevil.Flags.MouseButton;
 import com.spvessel.spacevil.Flags.SizePolicy;
 
 class TabBar extends Prototype implements IHLayout {
@@ -72,17 +72,24 @@ class TabBar extends Prototype implements IHLayout {
     private void initTab(Tab tab) {
         tab.view.setItemName(tab.getItemName() + "_view");
         tab.eventMousePress.add((sender, args) -> {
-            onTop(tab);
-            unselectOthers(tab, args);
+            if (args.button.equals(MouseButton.ButtonLeft)) {
+                onTop(tab);
+                unselectOthers(tab, args);
+            }
         });
         tab.eventTabRemove.add(() -> {
             selectBestRightoverTab();
+            // removeTab(tab);
         });
         tab.eventMouseDrop.add((sender, args) -> {
-            onTabDrop(tab, args);
+            if (args.button.equals(MouseButton.ButtonLeft)) {                
+                onTabDrop(tab, args);
+            }
         });
         tab.eventMouseDrag.add((sender, args) -> {
-            onTabDrag(tab, args);
+            if (args.button.equals(MouseButton.ButtonLeft)) {
+                onTabDrag(tab, args);
+            }
         });
     }
 
